c9d5bcfeb3e26b0761330640613aa403148eb0f4
nisse <nisse.malmstrom@icloud.com>
2026-02-25 15:44:37 +0100

    actually i want to add this test command to, tried experimenting with requirements

--- src/main/java/io/github/nissemanen/notsoSimpleClaims/NotsoSimpleClaims.java
+++ src/main/java/io/github/nissemanen/notsoSimpleClaims/NotsoSimpleClaims.java
@@ -1,6 +1,8 @@
 package io.github.nissemanen.notsoSimpleClaims;
 
+import io.github.nissemanen.notsoSimpleClaims.commands.DevCommands;
 import io.github.nissemanen.notsoSimpleClaims.listeners.PlayerListener;
+import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class NotsoSimpleClaims extends JavaPlugin {
@@ -8,6 +10,8 @@ public final class NotsoSimpleClaims extends JavaPlugin {
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(new PlayerListener(), this);
+
+        getLifecycleManager().registerEventHandler(LifecycleEvents.COMMANDS, commands -> new DevCommands().register(commands.registrar()));
     }
 
     @Override
--- src/main/java/io/github/nissemanen/notsoSimpleClaims/commands/DevCommands.java
+++ src/main/java/io/github/nissemanen/notsoSimpleClaims/commands/DevCommands.java
@@ -1,12 +1,13 @@
 package io.github.nissemanen.notsoSimpleClaims.commands;
 
-import com.mojang.brigadier.Command;
 import io.papermc.paper.command.brigadier.Commands;
+
+import java.util.Objects;
 
 public class DevCommands {
     public void register(Commands registrar) {
         registrar.register(
-                Commands.literal("DevClaims")
+                Commands.literal("DevClaims").requires(cmd -> Objects.requireNonNull(cmd.getExecutor()).hasPermission("NotsoSimpleClaims.DevCommands"))
                         .then(Commands.literal("claim"))
                         .then(Commands.literal("abandon"))
                         .build()
