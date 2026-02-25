733aed6540dbfc1e4dfa9162dfa8253894e35547
nisse <nisse.malmstrom@icloud.com>
2026-02-25 16:07:11 +0100

    uh, alr i got a nullpointer in the DevCommands cause i used cmd.getExecutor() instead of cmd.getSender()

--- src/main/java/io/github/nissemanen/notsoSimpleClaims/commands/DevCommands.java
+++ src/main/java/io/github/nissemanen/notsoSimpleClaims/commands/DevCommands.java
@@ -7,7 +7,7 @@ import java.util.Objects;
 public class DevCommands {
     public void register(Commands registrar) {
         registrar.register(
-                Commands.literal("DevClaims").requires(cmd -> Objects.requireNonNull(cmd.getExecutor()).hasPermission("NotsoSimpleClaims.DevCommands"))
+                Commands.literal("DevClaims").requires(cmd -> cmd.getSender().hasPermission("NotsoSimpleClaims.DevCommands"))
                         .then(Commands.literal("claim"))
                         .then(Commands.literal("abandon"))
                         .build()
--- src/main/java/io/github/nissemanen/notsoSimpleClaims/listeners/PlayerListener.java
+++ src/main/java/io/github/nissemanen/notsoSimpleClaims/listeners/PlayerListener.java
@@ -1,6 +1,9 @@
 package io.github.nissemanen.notsoSimpleClaims.listeners;
 
 import org.bukkit.Bukkit;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockState;
+import org.bukkit.block.TileState;
 import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -9,6 +12,15 @@ import org.bukkit.event.player.PlayerInteractEvent;
 public class PlayerListener implements Listener {
     @EventHandler
     final void onRightClickBlock(PlayerInteractEvent e) {
-        Bukkit.getLogger().info("clicked block: "+e.getClickedBlock());
+        Block block = e.getClickedBlock();
+        assert block != null;
+        BlockState blockState = block.getState();
+
+        if (!(blockState instanceof TileState tileState)) {
+            Bukkit.getLogger().info("clicked block is not a TileEntity");
+            return;
+        }
+
+        Bukkit.getLogger().info("\nclicked block: "+block+"\nblock PDC: "+tileState.getPersistentDataContainer());
     }
 }
