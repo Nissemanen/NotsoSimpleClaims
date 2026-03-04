987def7765483e7e6296b4b758baab9fb188a712
nisse <nisse.malmstrom@icloud.com>
2026-03-04 20:55:23 +0100

    just a small lil thing, i forgot to push this yesterday and couldnt code in school which means i actually had to pay attention in class :(

--- src/main/java/io/github/nissemanen/notsoSimpleClaims/Blocks/listeners/PlayerListenerCapitalBlock.java
+++ src/main/java/io/github/nissemanen/notsoSimpleClaims/Blocks/listeners/PlayerListenerCapitalBlock.java
@@ -1,12 +1,16 @@
 package io.github.nissemanen.notsoSimpleClaims.Blocks.listeners;
 
 import org.bukkit.Location;
+import org.bukkit.NamespacedKey;
+import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.plugin.Plugin;
 
 import java.util.HashMap;
@@ -14,8 +18,7 @@ import java.util.Map;
 import java.util.UUID;
 
 public class PlayerListenerCapitalBlock implements Listener {
-    @SuppressWarnings("unused")
-    private final Map<Location, UUID> capitalBlocks = new HashMap<>();
+    private final Map<Block, UUID> capitalBlocks = new HashMap<>();
     private final Plugin plugin;
 
     public PlayerListenerCapitalBlock(Plugin plugin) {
@@ -36,6 +39,12 @@ public class PlayerListenerCapitalBlock implements Listener {
         plugin.getLogger().info("placed");
 
         ItemStack item = e.getItemInHand();
+        PersistentDataContainer container = item.getItemMeta().getPersistentDataContainer();
+        NamespacedKey key = new NamespacedKey(plugin, "is_capital_block");
+
+        if (!(container.has(key) && Boolean.TRUE.equals(container.get(key, PersistentDataType.BOOLEAN)))) return;
+
+        capitalBlocks.put(e.getBlockPlaced(), e.getPlayer().getUniqueId());
 
         e.getPlayer().sendMessage("item:\n"+item);
     }
